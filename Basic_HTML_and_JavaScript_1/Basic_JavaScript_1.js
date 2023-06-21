12a857571a1be3aabcc250b7072fa7f28d45bc4c
Awaterlander <131086507+Awaterlander@users.noreply.github.com>
2023-06-20 21:10:39 -0500

    BASIC HTML AND JAVASCRIPT 1 SUBMISSION ASSIGNMENT

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<script src="Hello_World.js"></script>
+	alert("Hello, World! I am an alert box!");
+	
+		function My_First_Function() {
+		var String = "Kissme, Im Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML=
+		result;
+	}
+</html>
---
+++ Basic_HTML_and_JavaScript_1/Hello_World.js
@@ -0,0 +1,6 @@
+alert("Hello, World! I am an alert box!");
+
+<!DOCTYPE HTML>
+<html>
+	<script src="Hello_World.js"></script>
+</html>
